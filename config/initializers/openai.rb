65e3e0696b0d76820456f98d36ba801a3f3ca5a9
Oliver Thoma <oliver.thoma@hey.com>
2023-09-26 01:25:10 +0200

    Do not set OpenAI api key in test env

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai.api_key!
-  config.organization_id = Rails.application.credentials.openai.organization_id!
+  unless Rails.env.test?
+    config.access_token = Rails.application.credentials.openai.api_key!
+    config.organization_id = Rails.application.credentials.openai.organization_id!
+  end
 end
