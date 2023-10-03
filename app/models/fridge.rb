4b86853f26089bf8b2e84b043c37fc636c72f891
Daniel Bengl <daniel@bengl.cz>
2023-10-03 11:43:09 +0200

    Refactor index page with partial, replace links with buttons

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -5,4 +5,8 @@ class Fridge < ApplicationRecord
 
   attribute :ingredients, Ingredient.to_array_type
   accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+  def ingredients_text
+    ingredients.map(&:name).join(', ')
+  end
 end
