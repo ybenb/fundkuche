5271d7fc68f6b6a004c17ce9aff5dc78fd8a719e
Pascal Andermatt <p.andermatt@me.com>
2023-10-03 11:55:24 +0200

    clean up image service

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,23 +1,10 @@
 # frozen_string_literal: true
 
 class ImageService
+  ULTRALYTICS_URL = 'https://api.ultralytics.com/v1/predict/R6nMlK6kQjSsQ76MPqQM'.freeze
+
   def call(image_path:)
-    uri = URI.parse('https://api.ultralytics.com/v1/predict/R6nMlK6kQjSsQ76MPqQM')
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = 'multipart/form-data'
-    request['x-api-key'] = Rails.application.credentials.ultralytics.api_key!
-
-    request.set_form([
-                       ['size', '640'],
-                       ['confidence', '0.05'],
-                       ['iou', '0.05'],
-                       ['image', File.open(resize_image(image_path))]
-                     ], 'multipart/form-data')
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    response = http.request(request)
+    response = send_request(image_path)
     response = JSON.parse(response.body)
 
     ingredients = response['data'].pluck('name')
@@ -26,6 +13,25 @@ class ImageService
   end
 
   private
+
+  def send_request(image_path)
+    uri = URI.parse(ULTRALYTICS_URL)
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = 'multipart/form-data'
+    request['x-api-key'] = Rails.application.credentials.ultralytics.api_key!
+
+    params = [
+      %w[size 640],
+      %w[confidence 0.05],
+      %w[iou 0.05],
+      ['image', File.open(resize_image(image_path))]
+    ]
+    request.set_form(params, 'multipart/form-data')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.request(request)
+  end
 
   def resize_image(image_path)
     image = MiniMagick::Image.open(image_path)
