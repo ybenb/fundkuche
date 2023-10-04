ee1dfc0a27b0620879ddd311273465054781d118
Oliver Thoma <oliver.thoma@hey.com>
2023-10-04 10:44:12 +0200

    Unique form field names

--- app/javascript/controllers/ingredients_controller.js
+++ app/javascript/controllers/ingredients_controller.js
@@ -3,7 +3,19 @@ import NestedForm from 'stimulus-rails-nested-form';
 export default class extends NestedForm {
   static targets = ['nameInput', 'quantityInput'];
 
+  uniqueId = 0;
+
   connect() {
     super.connect();
+  }
+
+  add(t) {
+    t.preventDefault();
+    const e = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, this.getUniqueId());
+    this.targetTarget.insertAdjacentHTML("beforebegin", e);
+  }
+
+  getUniqueId() {
+    return `${new Date().getTime()}-${this.uniqueId++}`;
   }
 }
