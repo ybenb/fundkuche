9499eb1c7116ec855bb96a6f22b0f3a7bf6ea179
Daniel Bengl <daniel@bengl.cz>
2023-10-04 10:10:30 +0200

    Refactor flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(type)
+    {
+      notice: 'alert alrert-info',
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-warning',
+      default: 'alert alert-info'
+    }[type.to_sym] || type.to_s
+  end
 end
