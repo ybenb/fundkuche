7b0dee3aacf7f59d821844655b9fe85a429fa6ef
Yessin Ben Brahim <yessin@Yessins-MacBook-Pro.local>
2026-04-01 15:50:41 +0200

    Fix SSL for Deploio (assume_ssl + force_ssl)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # assume_ssl tells Rails the load balancer has already terminated SSL (required on Deploio).
+  config.assume_ssl = true
   config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
